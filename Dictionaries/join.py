daf57b3220ab334f322ee440cfaf9e133e59b765
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-07-05 17:46:42 -0700

    Adds directional game code

--- Dictionaries/join.py
+++ Dictionaries/join.py
@@ -1,11 +1,40 @@
 locations = {0: "You are sitting in front of a computer learning Python",
-             1: "You are standing at the end of a road before a small brick building ,"
+             1: "You are standing at the end of a road before a small brick building",
+             2: "You are at the top of a hill",
+             3: "You are inside of a building, a well house for a small stream",
+             4: "You are om a valley beside a stream",
+             5: "You are in the forest"
+             }
+exits = [{"Q", 0},
+         {"W": 2, "E": 3, "N": 5, "Q": 0},
+         {"N": 5, "Q": 0},
+         {"W": 1, "Q": 0},
+         {"N": 1, "W": 2, "Q": 0},
+         {"N": 2, "S": 1, "Q": 0}
+        ]
+loc = 1
+while True:
+    availableExits = ""
+    for direction in exits[loc].keys():
+        availableExits += direction + ", "
 
-}
+
+    print(locations[loc])
+
+    if loc == 0:
+        break
+    direction = input("Available Exits are " + availableExits).upper()
+    print()
+    if direction in exits[loc]:
+        loc = exits[loc][direction]
+    else:
+        print("You cannot go in that direction")
 
 
 
-# mylist = ["a", "b", "c", "d"]
+
+# while True:
+#     mylist = ["a", "b", "c", "d"]
 # letters = ["abcdefghijklmnopqrstuvwxyz"]
 # numbers = "123456789"
 #
@@ -14,3 +43,4 @@ locations = {0: "You are sitting in front of a computer learning Python",
 #     newString = " mississippi ".join(numbers)
 #
 # print(newString)
+
