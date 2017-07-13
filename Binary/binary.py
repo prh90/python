39827ae7f87084fda451a21c24a35d070608b5f4
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-07-12 19:59:06 -0700

    Adds new folder to work on binary creation and same as IO write, read

--- Binary/binary.py
+++ Binary/binary.py
@@ -1,18 +1,7 @@
-# # This code is for Binary
-# for i in range(17):
-#     print("{0:>2} in binary is {0:>08b}".format(i))
-#
-# print("*"*50)
-#
-# # This code is for Hexadecimal
-# for i in range(17):
-#     print("{0:>2} in Hex is {0:>02x}".format(i))
+with open("binary", 'bw') as bin_file:
+    for i in range(17):
+        bin_file.write(bytes([i]))
 
-
-x = 0x20
-y = 0x0a
-
-print(x)
-print(y)
-print(x + y)
-print(0b1011110000)
+with open("binary", 'br') as binFile:
+    for b in binFile:
+        print(b)
