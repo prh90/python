8288cb94e07ac6a11821ec364bf29fea1d777548
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-07-13 18:54:18 -0700

    Adds code to save into binary and code to retrieve and read back saved binary code

--- Binary/binary.py
+++ Binary/binary.py
@@ -1,7 +1,34 @@
-with open("binary", 'bw') as bin_file:
-    for i in range(17):
-        bin_file.write(bytes([i]))
+# with open("binary", 'bw') as bin_file:
+#     bin_file.write(bytes(range(17)))
+#     # for i in range(17):
+#     #     bin_file.write(bytes([i]))
+# #     Can be replaced with line of code in line 2
+#
+# with open("binary", 'br') as binFile:
+#     for b in binFile:
+#         print(b)
 
-with open("binary", 'br') as binFile:
-    for b in binFile:
-        print(b)
+a = 65534 # FF FE
+b = 65535 # FF FF
+c = 65536 # 00 01 00 00
+x = 2998302 # 02 2D C0 1E
+
+with open("binary2", 'bw') as bin_file:
+    bin_file.write(a.to_bytes(2, 'big'))
+    bin_file.write(b.to_bytes(2, 'big'))
+    bin_file.write(c.to_bytes(4, 'big'))
+    bin_file.write(x.to_bytes(4, 'big'))
+    bin_file.write(x.to_bytes(4, 'little'))
+
+with open("binary2", 'br') as bin_file:
+    e = int.from_bytes(bin_file.read(2), 'big')
+    print(e)
+    f = int.from_bytes(bin_file.read(2), 'big')
+    print(f)
+    g = int.from_bytes(bin_file.read(4), 'big')
+    print(g)
+    h = int.from_bytes(bin_file.read(4), 'big')
+    print(h)
+    i = int.from_bytes(bin_file.read(4), 'big')
+    print(i)
+
