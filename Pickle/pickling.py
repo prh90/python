647b808bebdd0117ab93f355b4ac5c8cecfcec5a
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-07-13 19:02:41 -0700

    Adds file to learn pickle

---
+++ Pickle/pickling.py
@@ -0,0 +1,8 @@
+import pickle
+imelda = ("More mayhem",
+          "Imelda May",
+          "2011",
+          ((1, "Pulling the rug"),
+           (2, "Psycho"),
+           (3, "Mayhem"),
+           (4, "Kentish Town Waltz")))
