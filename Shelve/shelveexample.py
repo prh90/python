7f3e103a1c5f61ee216e61cd14bc2c60f0379cf7
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-07-14 20:03:00 -0700

    Adds order of the fruit in shelf

--- Shelve/shelveexample.py
+++ Shelve/shelveexample.py
@@ -21,23 +21,28 @@ import shelve
 # This is the manual way to open and close
 
 fruit = shelve.open('ShelfTest')
-fruit['orange'] = "a sweet, orange, citrus, fruit"
-fruit['apple'] = "good for making cider"
-fruit['lemon'] = "a sour , yellow citrus fruit"
-fruit['grape'] = "a small, sweet fruit growing in bunches"
-fruit['lime'] = "a sour, green citrus fruit"
+# once you save it into shelf then when you rerun it will rewrite the shelf
+
+# fruit['orange'] = "a sweet, orange, citrus, fruit"
+# fruit['apple'] = "good for making cider"
+# fruit['lemon'] = "a sour , yellow citrus fruit"
+# fruit['grape'] = "a small, sweet fruit growing in bunches"
+# fruit['lime'] = "a sour, green citrus fruit"
 
 # print(fruit["lemon"])
 # print(fruit["grape"])
-# this is to explicitly tell it to close
-while True:
-    dict_key = input("please enter a fruit: ")
-    if dict_key == "quit":
-        break
-    if dict_key in fruit:
-        description = fruit[dict_key]
-        print(description)
-    else:
-        print("We don't have {}".format(dict_key))
+# # this is to explicitly tell it to close
+# while True:
+#     dict_key = input("please enter a fruit: ")
+#     if dict_key == "quit":
+#         break
+#     if dict_key in fruit:
+#         description = fruit[dict_key]
+#         print(description)
+#     else:
+#         print("We don't have {}".format(dict_key))
+
+for f in fruit:
+    print(f + " - " + fruit[f])
 
 fruit.close()
