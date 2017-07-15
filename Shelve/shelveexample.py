09dbd4f570dbc47daf0b9252b5008ffe98a4fada
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-07-14 19:45:49 -0700

    Adds fruit shelve, having user input response and refactor

--- Shelve/shelveexample.py
+++ Shelve/shelveexample.py
@@ -1,27 +1,43 @@
 import shelve
 
 # A shelve is a read/write by nature
-with shelve.open('ShelfTest') as fruit:
-    fruit['orange'] = "a sweet, orange, citrus, fruit"
-    fruit['apple'] = "good for making cider"
-    fruit['lemon'] = "a sour , yellow citrus fruit"
-    fruit['grape'] = "a small, sweet fruit growing in bunches"
-    fruit['lime'] = "a sour, green citrus fruit"
+# with shelve.open('ShelfTest') as fruit:
+#     fruit['orange'] = "a sweet, orange, citrus, fruit"
+#     fruit['apple'] = "good for making cider"
+#     fruit['lemon'] = "a sour , yellow citrus fruit"
+#     fruit['grape'] = "a small, sweet fruit growing in bunches"
+#     fruit['lime'] = "a sour, green citrus fruit"
+#
+#     print(fruit["lemon"])
+#     print(fruit["grape"])
+#
+#     fruit["lime"] = "great with tequila"
+#
+#     for snack in fruit:
+#         print(snack + ": " + fruit[snack])
 
-    print(fruit["lemon"])
-    print(fruit["grape"])
 # If code is outside the with statement you receive an error because the 'with' closes the loop as soon as block ends
 #############################################
 # This is the manual way to open and close
 
-# fruit = shelve.open('ShelfTest')
-# fruit['orange'] = "a sweet, orange, citrus, fruit"
-# fruit['apple'] = "good for making cider"
-# fruit['lemon'] = "a sour , yellow citrus fruit"
-# fruit['grape'] = "a small, sweet fruit growing in bunches"
-# fruit['lime'] = "a sour, green citrus fruit"
-#
+fruit = shelve.open('ShelfTest')
+fruit['orange'] = "a sweet, orange, citrus, fruit"
+fruit['apple'] = "good for making cider"
+fruit['lemon'] = "a sour , yellow citrus fruit"
+fruit['grape'] = "a small, sweet fruit growing in bunches"
+fruit['lime'] = "a sour, green citrus fruit"
+
 # print(fruit["lemon"])
 # print(fruit["grape"])
 # this is to explicitly tell it to close
-# fruit.close()
+while True:
+    dict_key = input("please enter a fruit: ")
+    if dict_key == "quit":
+        break
+    if dict_key in fruit:
+        description = fruit[dict_key]
+        print(description)
+    else:
+        print("We don't have {}".format(dict_key))
+
+fruit.close()
