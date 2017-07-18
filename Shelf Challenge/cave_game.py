e7cca85ca4b4eb294a500e202ef87f68d10b5336
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-07-17 21:23:02 -0700

    Adds working game with shelve

--- Shelf Challenge/cave_game.py
+++ Shelf Challenge/cave_game.py
@@ -1,19 +1,21 @@
 import shelve
 
-game = shelve.open("game")
-loc = 1
+locations = shelve.open("locations")
+vocabulary = shelve.open("vocabulary")
+
+loc = '1'
 while True:
     # UPdated to master dictionary so locations and get the exit keys
-    availableExits = ", ".join(game["locations"][loc]["exits"].keys())
+    availableExits = ", ".join(locations[loc]["exits"].keys())
     # same here
-    print(game["locations"][loc]["desc"])
+    print(locations[loc]["desc"])
 
-    if loc == 0:
+    if loc == '0':
         break
     else:
-        # Also here same principal
-        allExits = game["locations"][loc]["exits"].copy()
-        allExits.update(game["locations"][loc]["namedExits"])
+        # Also here same principalq
+        allExits = locations[loc]["exits"].copy()
+        allExits.update(locations[loc]["namedExits"])
 
     direction = input("Available exits are " + availableExits + " ").upper()
     print()
@@ -22,8 +24,8 @@ while True:
     if len(direction) > 1:  # more than 1 letter, so check vocab
         words = direction.split()
         for word in words:
-            if word in game["vocabulary"]:   # does it contain a word we know?
-                direction = game["vocabulary"][word]
+            if word in vocabulary:   # does it contain a word we know?
+                direction = vocabulary[word]
                 break
 
     if direction in allExits:
@@ -31,4 +33,5 @@ while True:
     else:
         print("You cannot go in that direction")
 
-game.close()
+vocabulary.close()
+locations.close()
--- Shelf Challenge/cave_initialize.py
+++ Shelf Challenge/cave_initialize.py
@@ -6,21 +6,22 @@ locations['0'] = {"desc": "You are sitting in front of a computer learning Pytho
                   "exits": {},
                   "namedExits": {}}
 locations['1'] = {"desc": "You are standing at the end of a road before a small brick building",
-                  "exits": {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
-                  "namedExits": {"2": 2, "3": 3, "5": 5, "4": 4}}
+                  "exits": {"W": '2', "E": '3', "N": '5', "S": '4', "Q": '0'},
+                  "namedExits": {"2": '2', "3": '3', "5": '5', "4": '4'}}
 locations['2'] = {"desc": "You are at the top of a hill",
-                  "exits": {"N": 5, "Q": 0},
-                  "namedExits": {"5": 5}}
+                  "exits": {"N": '5', "Q": '0'},
+                  "namedExits": {"5": '5'}}
 locations['3'] = {"desc": "You are inside a building, a well house for a small stream",
-                  "exits": {"W": 1, "Q": 0},
-                  "namedExits": {"1": 1}}
+                  "exits": {"W": '1', "Q": '0'},
+                  "namedExits": {"1": '1'}}
 locations['4'] = {"desc": "You are in a valley beside a stream",
-                  "exits": {"N": 1, "W": 2, "Q": 0},
-                  "namedExits": {"1": 1, "2": 2}}
+                  "exits": {"N": '1', "W": '2', "Q": '0'},
+                  "namedExits": {"1": '1', "2": '2'}}
 locations['5'] = {"desc": "You are in the forest",
-                  "exits": {"W": 2, "S": 1, "Q": 0},
-                  "namedExits": {"2": 2, "1": 1}}
+                  "exits": {"W": '2', "S": '1', "Q": '0'},
+                  "namedExits": {"2": '2', "1": '1'}}
 locations.close()
+# converted the integers to the data type I am inputting string
 
 
 vocabulary = shelve.open("vocabulary")
