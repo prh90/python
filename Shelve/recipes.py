780b74ad1a1c25a6e6e1883aba46256df3f47a5e
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-07-17 18:10:02 -0700

    Adds an append to list in shelve

--- Shelve/recipes.py
+++ Shelve/recipes.py
@@ -1,13 +1,29 @@
 import shelve
 
-blt = ["bacon", "lettuce", "tomato", "bread"]
-beans_on_toast = ["beans", "bread"]
-scrambled_eggs = ["eggs", "butter", "milk"]
+# blt = ["bacon", "lettuce", "tomato", "bread"]
+# beans_on_toast = ["beans", "bread"]
+# scrambled_eggs = ["eggs", "butter", "milk"]
 soup = ["Tin of soup"]
-pasta = ["Pasta", "Cheese"]
+# pasta = ["Pasta", "Cheese"]
 
 with shelve.open('recipes') as recipes:
-    recipes["blt"] = blt
-    recipes["beans on toast"] = beans_on_toast
-    recipes["scrabled eggs"] = scrambled_eggs
-    recipes["pasta"] = pasta
+    # recipes["blt"] = blt
+    # recipes["beans on toast"] = beans_on_toast
+    # recipes["scrabled eggs"] = scrambled_eggs
+    # recipes["pasta"] = pasta
+    # recipes["soup"] = soup
+
+    # recipes["blt"].append("butter")
+    # recipes["pasta"].append("tomato")
+
+    # Now I can comment out this code since it updated the list
+    # temp_list = recipes["blt"]
+    # temp_list.append("butter")
+    # recipes["blt"] = temp_list
+    #
+    # temp_list = recipes["pasta"]
+    # temp_list.append("tomato")
+    # recipes["pasta"] = temp_list
+
+    for snack in recipes:
+        print(snack, recipes[snack])
