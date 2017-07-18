c58a92121fcf34467a096884f52c5f708eee18cd
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-07-17 22:30:07 -0700

    adds file to practice import of a module, in this case turtle and different ways to use the module

---
+++ modimport/turtledemo.py
@@ -0,0 +1,17 @@
+# import turtle
+# instead of importing everything
+# we can just import the ones we need
+# from turtle import forward, right, left, done, circle
+from turtle import *
+
+fin = "Well done, you have finished the drawing"
+
+forward(150)
+right(250)
+forward(150)
+circle(20)
+# turtle.left(200)
+# turtle.forward(150)
+
+done()
+print(fin)
