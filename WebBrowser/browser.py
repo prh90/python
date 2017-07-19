9f0e24a17f33b387629cfa05cbb7c949d4019fd9
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-07-18 18:43:08 -0700

    Adds ways to open new window

---
+++ WebBrowser/browser.py
@@ -0,0 +1,13 @@
+import webbrowser
+#
+# webbrowser.open("https://www.python.org", new=1)
+#
+# help(webbrowser)
+
+# for i in range(10):
+#     print(i)
+# chrome = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s"
+# webbrowser.get(chrome).open_new_tab("https://www.python.org")
+# chrome.open("https://www.python.org")
+# print(webbrowser._browsers)
+# I need to look into registering chrome as runnable browser, it only works with internet explorer
