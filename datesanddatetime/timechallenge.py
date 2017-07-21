0adf75c81e75b31a3164ef29f08e56db06ac4675
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-07-20 18:04:34 -0700

    Adds code to display the options

--- datesanddatetime/timechallenge.py
+++ datesanddatetime/timechallenge.py
@@ -2,18 +2,35 @@ import time
 import pytz
 # import random
 
-# # Prints the whole list of acceptable timezones
-# for x in pytz.all_timezones:
-#     print(x)
+
 all_timezones = ['Africa/Johannesburg', 'America/Chicago', 'Pacific/Auckland', 'Japan', 'Europe/Zurich', 'Europe/Sarajevo', 'Europe/Rome', 'Europe/Moscow', 'Asia/Gaza']
 
-for x in all_timezones:
-    print("{}: {}".format(all_timezones.index(x) + 1, x))
-print("0: Exit")
 
-choice = input("Choose one to see the time: ")
+# for x in all_timezones:
+#     print("{}: {}".format(all_timezones.index(x) + 1, x))
+tim1 = 'Africa/Johannesburg'
+tim2 = 'America/Chicago'
+tim3 = 'Pacific/Auckland'
+tim4 = 'Japan'
+tim5 = 'Europe/Zurich'
+tim6 = 'Europe/Sarajevo'
+tim7 = 'Europe/Rome'
+tim8 = 'Europe/Moscow'
+tim9 = 'Asia/Gaza'
+l_exit = 'Exit'
 
-if choice == 0:
-    
-else:
-    print("OK")
+l_list = [tim1, tim2, tim3, tim4, tim5, tim6, tim7, tim8, tim9, l_exit]
+
+for x in l_list:
+    if x == l_exit:
+        print("0: Exit")
+    else:
+        print("{}: {}".format(l_list.index(x) + 1, x))
+
+print()
+int1 = input("Select the timezone you would like to see: ")
+print(int1)
+
+
+
+
