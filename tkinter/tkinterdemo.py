33c483cec6569fc8fc165060808f44ee02f76c67
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-07-21 17:12:04 -0700

    Adds tkinter file to practice with GUI

--- tkinter/tkinterdemo.py
+++ tkinter/tkinterdemo.py
@@ -6,4 +6,22 @@ except ImportError: # python 2
 print(tkinter.TkVersion)
 print(tkinter.TclVersion)
 
-tkinter._test()
+# tkinter._test()
+mainWindow = tkinter.Tk()
+
+mainWindow.title("Hello World")
+mainWindow.geometry('640x480+8+400')
+
+label = tkinter.Label(mainWindow, text="Hello World")
+label.pack(side='top')
+
+canvas = tkinter.Canvas(mainWindow, relief='raised', borderwidth=1)
+canvas.pack(side='left')
+button1 = tkinter.Button(mainWindow, text='button1')
+button2 = tkinter.Button(mainWindow, text='button2')
+button3 = tkinter.Button(mainWindow, text='button3')
+button1.pack(side='left')
+button2.pack(side='left')
+button3.pack(side='left')
+
+mainWindow.mainloop()
