a08c98f2c42ee8820da8aebe50a6252209548f8a
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-07-25 22:06:00 -0700

    Adds simple layout of grid

--- tkinter/screen.py
+++ tkinter/screen.py
@@ -11,4 +11,21 @@ mainWindow.geometry('640x480+8+200')
 label = tkinter.Label(mainWindow, text='Tkinter Grid Demo')
 label.grid(row=0, column=0, columnspan=3)
 
+mainWindow.columnconfigure(0, weight=1)
+mainWindow.columnconfigure(1, weight=1)
+mainWindow.columnconfigure(2, weight=3)
+mainWindow.columnconfigure(3, weight=3)
+mainWindow.columnconfigure(4, weight=3)
+mainWindow.rowconfigure(0, weight=1)
+mainWindow.rowconfigure(1, weight=10)
+mainWindow.rowconfigure(2, weight=1)
+mainWindow.rowconfigure(3, weight=3)
+mainWindow.rowconfigure(4, weight=3)
+
+fileList = tkinter.Listbox(mainWindow)
+fileList.grid(row=1, column=0, sticky='nsew', rowspan=2)
+fileList.config(border=2, relief='sunken')
+
+
+
 mainWindow.mainloop()
