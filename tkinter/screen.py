9842d7d962088b15087eba5bbaed1bc5512befbd
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-07-28 17:52:38 -0700

    Adds time spinners as well as date spinners

--- tkinter/screen.py
+++ tkinter/screen.py
@@ -59,7 +59,7 @@ result.grid(row=2, column=2, sticky='sw')
 
 # Frame for the time spinners
 timeFrame = tkinter.LabelFrame(mainWindow, text='Time')
-timeFrame.grid(row=0, column=0, sticky='new')
+timeFrame.grid(row=3, column=0, sticky='new')
 # time spinners
 hourSpinner = tkinter.Spinbox(timeFrame, width=2, value=tuple(range(0, 24)))
 minuteSpinner = tkinter.Spinbox(timeFrame, width=2, from_=0, to=59)
@@ -70,7 +70,28 @@ tkinter.Label(timeFrame, text=':').grid(row=0, column=1)
 minuteSpinner.grid(row=0, column=2)
 tkinter.Label(timeFrame, text=":").grid(row=0, column=3)
 secondSpinner.grid(row=0, column=4)
+timeFrame['padx'] = 36
+# timeFrame['pady'] = 15
 
+# Frame for the date spinners
+dateFrame = tkinter.Frame(mainWindow)
+dateFrame.grid(row=4, column=0, sticky='new')
+# Date labels
+dayLabel = tkinter.Label(dateFrame, text='Day')
+monthLabel = tkinter.Label(dateFrame, text='Month')
+yearLabel = tkinter.Label(dateFrame, text='Year')
+dayLabel.grid(row=0, column=0, sticky='w')
+monthLabel.grid(row=0, column=1, sticky='w')
+yearLabel.grid(row=0, column=2, sticky='w')
+
+# Date spinners
+
+daySpin = tkinter.Spinbox(dateFrame, width=5, from_=1, to=31)
+monthSpin = tkinter.Spinbox(dateFrame, width=5, values=("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"))
+yearSpin = tkinter.Spinbox(dateFrame, width=5, from_=2000, to=2099)
+daySpin.grid(row=1, column=0, sticky='w')
+monthSpin.grid(row=1, column=1, sticky='w')
+yearSpin.grid(row=1, column=2, sticky='w')
 
 mainWindow.mainloop()
 
