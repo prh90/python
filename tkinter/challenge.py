3ddfb0fcd0a30b50d4748bd88278094e6f536369
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-07-31 20:00:05 -0700

    Adds code to try and fix the broken entry

--- tkinter/challenge.py
+++ tkinter/challenge.py
@@ -6,23 +6,26 @@ mainWindow.title("Calculator")
 mainWindow.geometry('640x480+8+200')
 
 for i in range(0, 4):
-    mainWindow.columnconfigure(i, weight=1)
+    mainWindow.columnconfigure(i, weight=100)
 
 for i in range(0, 5):
     if i == 1:
-        mainWindow.rowconfigure(1, weight=100)
+        mainWindow.rowconfigure(1, weight=600)
     else:
-        mainWindow.rowconfigure(i, weight=1)
+        mainWindow.rowconfigure(i, weight=1000)
 
 
 result = tkinter.Entry(mainWindow)
 result.grid(row=0, column=2, columnspan=1, sticky='nsew')
 
-btn = ["C", "CE", "7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "0", "=", "/"]
-
-for i in btn:
-    button = tkinter.Button(mainWindow, text=i)
-    # for m in len(btn):
-    #     button.grid(row=1, column=2, columnspan=1, sticky='w')
+cButton = tkinter.Button(mainWindow, text='C')
+cButton.grid(row=1, column=0, sticky='e')
+#
+# btn = ["C", "CE", "7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "0", "=", "/"]
+#
+# for i in btn:
+#     button = tkinter.Button(mainWindow, text=i)
+#     for m in range(0, len(btn)):
+#         button.grid(row=m, column=m, columnspan=1, sticky='w')
 
 mainWindow.mainloop()
