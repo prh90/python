33e346f4bb0142af69f0a40dfc3ec0b3469d80d7
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-08-02 21:14:09 -0700

    Adds 2 functions with ability to either print them on the spot or open them in new file

--- functions/functions.py
+++ functions/functions.py
@@ -5,16 +5,17 @@ def python_food():
     print(" " * left_margin, text)
 
 
-def centre_text(*args, sep=' ', end='\n', file=None, flush=False):
+def centre_text(*args, sep=' '):
     text = ""
     for arg in args:
         text += str(arg) + sep
     left_margin = (80-len(text)) // 2
-    print(" " * left_margin, text, end=end, file=file, flush=flush)
+    return " " * left_margin + text
+
+# with open("centered", mode="w") as centred_file:
 
 # call function
-# python_food()
-centre_text("Aunt Jemima")
-centre_text("Uncle Roger")
-centre_text(12)
-centre_text("first", "second21", 3, 4, "spam", sep=":")
+print(centre_text("Aunt Jemima"))
+print(centre_text("Uncle Roger"))
+print(centre_text(12))
+print(centre_text("first", "second21", 3, 4, "spam", sep=":"))
