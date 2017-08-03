d4e95524278d2ec15e3e6f69d9382f07b723aa04
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-08-02 20:42:11 -0700

    Adds args to pass into function and better way to let anything to be input into parameter

--- functions/functions.py
+++ functions/functions.py
@@ -5,15 +5,16 @@ def python_food():
     print(" " * left_margin, text)
 
 
-def centre_text(text):
-    text = str(text)
+def centre_text(*args, sep=' ', end='\n', file=None, flush=False):
+    text = ""
+    for arg in args:
+        text += str(arg) + sep
     left_margin = (80-len(text)) // 2
-    print(" " * left_margin, text)
+    print(" " * left_margin, text, end=end, file=file, flush=flush)
 
 # call function
 # python_food()
-# centre_text("Aunt Jemima")
-# centre_text("Uncle Roger")
-# centre_text(12)
-
-print("first", "second", 3, 4, "spam")
+centre_text("Aunt Jemima")
+centre_text("Uncle Roger")
+centre_text(12)
+centre_text("first", "second21", 3, 4, "spam", sep=":")
