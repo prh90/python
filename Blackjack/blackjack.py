84f7871d726c3b4605ed7c4656fc2965afa16ed5
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-08-11 22:00:48 -0700

    Adds deck of cards converted from svg to png and adds code to display

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -1,4 +1,4 @@
-import random
+# import random
 
 try:
     import tkinter
@@ -6,6 +6,29 @@ except ImportError:
     import Tkinter as tkinter
 
 mainWindow = tkinter.Tk()
+
+
+def load_images(card_images):
+    suits = ['hearts', 'clubs', 'diamonds', 'spades']
+    face_cards = ['jack', 'queen', 'king']
+
+    if tkinter.TkVersion >= 8.6:
+        extension = 'png'
+    else:
+        extension = 'png'
+
+#     for each suit, retrieve the image for the cards
+    for suit in suits:
+        # first the number cards 1 to 10
+        for card in range(1, 11):
+            name = 'cards/{}_{}.{}'.format(str(card), suit, extension)
+            image = tkinter.PhotoImage(file=name)
+            card_images.append((card, image,))
+
+        for card in face_cards:
+            name = 'cards/{}_{}.{}'.format(str(card), suit, extension)
+            image = tkinter.PhotoImage(file=name)
+            card_images.append((10, image,))
 
 # Set up the screen and frames for the dealer and player
 
@@ -21,13 +44,29 @@ card_frame.grid(row=1, column=0, sticky="ew", columnspan=3, rowspan=2)
 dealer_score_label = tkinter.IntVar()
 tkinter.Label(card_frame, text="Dealer", background="green", fg="white").grid(row=0, column=0)
 tkinter.Label(card_frame, text=dealer_score_label, background="green", fg="white").grid(row=1, column=0)
-# embedded frame hold the card images
+# embedded frame to hold the card images
 dealer_card_frame = tkinter.Frame(card_frame, background="green")
 dealer_card_frame.grid(row=0, column=1, sticky="ew", rowspan=2)
 
 player_score_label = tkinter.IntVar()
 tkinter.Label(card_frame, text="Player", background="green", fg="white").grid(row=2, column=0)
 tkinter.Label(card_frame, textvariable=player_score_label, background="green", fg="white").grid(row=3, column=0)
+# embedded frame to hold the card images
+player_card_frame = tkinter.Frame(card_frame, background="green")
+player_card_frame.grid(row=2, column=1, sticky="ew", rowspan=2)
 
+button_frame = tkinter.Frame(mainWindow)
+button_frame.grid(row=3, column=0, columnspan=3, sticky='w')
+
+dealer_button = tkinter.Button(button_frame, text="Dealer")
+dealer_button.grid(row=0, column=0)
+
+player_button = tkinter.Button(button_frame, text="Player")
+player_button.grid(row=0, column=1)
+
+# load cards
+cards = []
+load_images(cards)
+print(cards)
+# create a new deck of cards and shuffle them.
 mainWindow.mainloop()
-
