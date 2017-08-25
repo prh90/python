c6c51c997af75f2fa62007ceedc742ef247971e8
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-08-24 22:36:51 -0700

    Adds final lesson on scopes and nested methods

--- ScopeAndNamespace/spam.py
+++ ScopeAndNamespace/spam.py
@@ -8,14 +8,17 @@ def spam1():
             print("In spam 3, locals are {}".format(locals()))
             return z
 
-        y = " more" + x
-        y += spam3()
+        y = " more " + x  # y must exist before spam3 is called
+        y += spam3()  # do not combine these assignments
         print("In spam 2, locals are {}".format(locals()))
         return y
 
-    x = "spam"
-    x += spam2()
+    x = "spam"  # must exist before spam2 is called
+    x += spam2()  # do not combine these assignments
     print("In spam 1, locals are {}".format(locals()))
     return x
 
 print(spam1())
+print(locals())
+print(globals())
+# In this scope locals and globals are the same thing
