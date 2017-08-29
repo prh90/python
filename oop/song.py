1add9d5eea3ed15ca404a70ac3ac5abd6b88f634
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-08-29 16:57:31 -0700

    Adds new file for new class

---
+++ oop/song.py
@@ -0,0 +1,16 @@
+class Song:
+    """ CLass to represent a song
+    Attributes:
+        title (str): The title of the song
+        artist(Artist): An artist object representing the songs creator
+        duration(int): The duration of the song in seconds. May be zero
+    """
+    def __init__(self, title, artist, duration=0):
+        """ Song init method
+
+        Args:
+            title(str): Initiates the 'title' attribute
+            artist(Artist): Artist object representing the songs creator.
+            duration(optional[int]): Initial value for the 'duration' attribute.
+                will default to zero if none is given
+            """
