05ffca6b3fadc7a837562fbe267bb45ebd3a04c1
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-09-06 00:53:04 -0700

    Adds STR to print out everything in the player attr

--- game_practice/Game/main.py
+++ game_practice/Game/main.py
@@ -2,21 +2,23 @@ from player import Player
 
 pablo = Player("Pablo")
 
-print(pablo.name)
-print(pablo.lives)
-print(pablo.level)
-print(pablo.score)
+# print(pablo.name)
+# print(pablo.lives)
+# print(pablo.level)
+# print(pablo.score)
 pablo.lives -= 1
-print(pablo.lives)
+print(pablo)
+
+pablo.lives -= 1
+print(pablo)
+
+pablo.lives -= 1
+print(pablo)
+
+pablo.lives -= 1
+print(pablo)
 
 # getter - gets values
-print(pablo.get_name())
+# print(pablo.get_name())
 # setter - sets values
-pablo.set_lives(300)
-
-
-dad = Player("Dad")
-print(dad.name)
-
-mom = Player("mom")
-print(mom.name)
+# pablo.set_lives(300)
