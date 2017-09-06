b4e4fdba822acbb4abbd920c28e28bf4f1dd9026
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-09-05 19:59:57 -0700

    Adds fixed code with allowing to find all artists by fixing wrong field on line 126

--- oop/song.py
+++ oop/song.py
@@ -104,50 +104,6 @@ class Artist:
         album_found.add_song(title)
 
 
-# def load_data():
-#     new_artist = None
-#     new_album = None
-#     artist_list = []
-#
-#     with open("albums.txt", "r") as albums:
-#         for line in albums:
-#             # data row should consist of (artist, album, year, song)
-#             artist_field, album_field, year_field, song_field = tuple(line.strip('\n').split('\t'))
-#             year_field = int(year_field)
-#             print("{}:{}:{}:{}".format(artist_field, album_field, year_field, song_field))
-#
-#             if new_artist is None:
-#                 new_artist = Artist(artist_field)
-#             elif new_artist.name != artist_field:
-#                 # we've just read details for a new artist
-#                 # store the current album in the current artist
-#                 # collection then create a new artist object
-#                 new_artist.add_album(new_album)
-#                 artist_list.append(new_artist)
-#                 new_artist = Artist(artist_field)
-#                 new_album = None
-#
-#             if new_album is None:
-#                 new_album = Album(album_field, year_field, new_artist)
-#             elif new_album.name != album_field:
-#                 # We've just read a new album for the current artist
-#                 # store current album in the artist's collection then create a new album object
-#                 new_artist.add_album(new_album)
-#                 new_album = Album(album_field, year_field, new_artist)
-#
-#             # create a new song object and add it to the current album's collection
-#             new_song = Song(song_field, new_artist)
-#             new_album.add_song(new_song)
-#
-#         # After reading the last line of the text file, we will have an artist
-#         # and album that haven't been store - process them now
-#         if new_artist is not None:
-#             if new_album is not None:
-#                 new_artist.add_album(new_album)
-#             artist_list.append(new_artist)
-#
-#     return artist_list
-
 def find_object(field, object_list):
     """ Check 'object_list' to see if an object with a 'name' attribute equal to 'field' exists, return it if so"""
     for item in object_list:
@@ -167,7 +123,7 @@ def load_data():
             year_field = int(year_field)
             print("{}:{}:{}:{}".format(artist_field, album_field, year_field, song_field))
 
-            new_artist = find_object(album_field, artist_list)
+            new_artist = find_object(artist_field, artist_list)
             if new_artist is None:
                 new_artist = Artist(artist_field)
                 artist_list.append(new_artist)
@@ -175,14 +131,6 @@ def load_data():
             new_artist.add_song(album_field, year_field, song_field)
 
     return artist_list
-
-# def create_checkfile(artist_list):
-#     """Create a check file from the object data for comparison with the original file"""
-#     with open("checkfile.txt", 'w') as checkfile:
-#         for new_artist in artist_list:
-#             for new_album in new_artist.albums:
-#                 for new_song in new_album.tracks:
-#                     print("{0.name}\t{1.name}\t{1.year}\t{2.title}".format(new_artist, new_album, new_song), file=checkfile)
 
 
 def create_checkfile(artist_list):
