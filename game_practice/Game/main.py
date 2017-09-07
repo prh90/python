f8d7571a750ebd2d4ec173b6daaae40c8e0dd382
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-09-06 18:18:54 -0700

    adds a scoring change when level increases

--- game_practice/Game/main.py
+++ game_practice/Game/main.py
@@ -18,6 +18,9 @@ print(pablo)
 pablo.lives -= 1
 print(pablo)
 
+# nothing in python is private, can be accessed, only thing we can do is rename it
+# If underlined and someone tries to access it, it can cause problems
+
 # getter - gets values
 # print(pablo.get_name())
 # setter - sets values
