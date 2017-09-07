d289667ed06c47c9e7ed0019d4f33da4be41602c
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-09-06 22:45:09 -0700

    Adds inheritance class, troll from enemy

--- game_practice/Game/main.py
+++ game_practice/Game/main.py
@@ -2,13 +2,16 @@ from player import Player
 
 pablo = Player("Pablo")
 
-from enemy import Enemy
+from enemy import Enemy, Troll
 
 random_monster = Enemy("Basic Enemy", 12, 1)
-print(random_monster)
+# print(random_monster)
+#
+# random_monster.take_damage(12)
+# print(random_monster)
 
-random_monster.take_damage(12)
-print(random_monster)
+ugly_troll = Troll("Trollski", 22, 4)
+print("Ugly troll - {}".format(ugly_troll))
 
 # nothing in python is private, can be accessed, only thing we can do is rename it
 # If underlined and someone tries to access it, it can cause problems
