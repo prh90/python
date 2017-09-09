11fd089611113f35ed4e4177d70c0846fe5e1179
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-09-09 01:34:53 -0700

    Adds composition practice by using HTML format as practice

--- game_practice/Game/ducks.py
+++ game_practice/Game/ducks.py
@@ -1,4 +1,20 @@
+class Wing(object):
+    def __init__(self, ratio):
+        self.ratio = ratio
+
+    def fly(self):
+        if self.ratio > 1:
+            print("Weee, this is fun")
+        elif self.ratio == 1:
+            print("This is hard work, but I'm flying")
+        else:
+            print("I think I'll just walk")
+
+
 class Duck(object):
+
+    def __init__(self):
+        self._wing = Wing(1.8)
 
     def walk(self):
         print("waddle, waddle, waddle")
@@ -8,6 +24,10 @@ class Duck(object):
 
     def quack(self):
         print("Quack Quack")
+
+    def fly(self):
+        self._wing.fly()
+
 
 
 class Penguin(object):
@@ -21,15 +41,16 @@ class Penguin(object):
         print("Are you 'avin' a larf? I'm a penguin")
 
 
-def test_duck(duck):
-    duck.walk()
-    duck.swim()
-    duck.quack()
+# def test_duck(duck):
+#     duck.walk()
+#     duck.swim()
+#     duck.quack()
 
 
 if __name__ == '__main__':
     donald = Duck()
-    test_duck(donald)
-
-    percy = Penguin()
-    test_duck(percy)
+    donald.fly()
+    # test_duck(donald)
+    #
+    # percy = Penguin()
+    # test_duck(percy)
