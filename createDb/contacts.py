48b6843df375f428a75af7e2b6115447a987c6b7
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-09-12 20:57:49 -0700

    Adds way to unpack tuple from database using python code

--- createDb/contacts.py
+++ createDb/contacts.py
@@ -1,5 +1,33 @@
 import sqlite3
 
 db = sqlite3.connect("contacts.sqlite")
-db.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)")
+db.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)")
 db.execute("INSERT INTO contacts(name, phone, email) VALUES('Pablo', 456789, 'pablo@email.com')")
+db.execute("INSERT INTO contacts VALUES ('Tim', 456123, 'tim@email.com')")
+
+
+cursor = db.cursor()
+cursor.execute("SELECT * FROM contacts")
+
+# print(cursor.fetchall())
+
+print(cursor.fetchone())
+print(cursor.fetchone())
+print(cursor.fetchone())
+
+for name, phone, email in cursor:
+    print(name)
+    print(phone)
+    print(email)
+    print("#"*40)
+
+# Once this for loop triggers the database ran through everything already and has nothing
+# else to print so it is highly efficient
+# for name, phone, email in cursor:
+#     print(name)
+#     print(phone)
+#     print(email)
+#     print("#"*40)
+cursor.close()
+db.close()
+
