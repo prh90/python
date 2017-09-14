6984860c9be0a4c947db6f1409171a8314b6dc9e
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-09-13 22:30:24 -0700

    NOTHING TO ADD

---
+++ ExceptionHandeling/challenge.py
@@ -0,0 +1,21 @@
+# def mult():
+#     num1 = input("Enter the first number: ")
+#     num2 = input("Now enter the second number: ")
+#
+#     print("{} / {} = {}".format(num1, num2, (int(num1)//int(num2))))
+#
+#
+# try:
+#     mult()
+# except ZeroDivisionError:
+#     print("Cant divide by zero son")
+###########################################################################
+
+def getint():
+    while True:
+        try:
+            number = int(input("Please enter a number: "))
+            return number
+        except ValueError:
+            print("Invalid number entered, please try again")
+            
