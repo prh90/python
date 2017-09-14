24c2938cbebf446690e35ef1df6ace3afa4f7cc8
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-09-13 20:13:28 -0700

    Adds definitions on error handling

--- ExceptionHandeling/examples.py
+++ ExceptionHandeling/examples.py
@@ -3,13 +3,17 @@ def factorial(n):
     if n<=1:
         return 1
     else:
+        # print(n / 0)
         return n * factorial(n-1)
 
 
 # print(factorial(25))
+# can combine errors if the message being returned can make sense for both else
+# use separate messages with error handling.
 try:
-    print(factorial(1000))
+    print(factorial(900))
 except RecursionError:
     print("This program can't calculate factorials that large.")
-
+except ZeroDivisionError:
+    print("Cant divide by zero son")
 print("Program terminated")
