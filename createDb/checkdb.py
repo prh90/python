8ea9619659cfe0712149041cde9596c5a2064b1e
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-09-13 18:42:10 -0700

    Adds two solutions that will return the result we are looking for

--- createDb/checkdb.py
+++ createDb/checkdb.py
@@ -2,15 +2,14 @@ import sqlite3
 
 conn = sqlite3.connect("contacts.sqlite")
 
-id_1 = input("Please enter the name of who you are looking for: ")
+name = input("Please enter the name of who you are looking for: ")
 
-# find = "SELECT * FROM contacts WHERE name = {}".format(name)
-# print(find)
-for name, phone, email in conn.execute("SELECT * FROM contacts"):
-    if name == id_1:
-        print("#"*40)
-        print("Name: {}".format(name))
-        print("Phone: {}".format(phone))
-        print("Email: {}".format(email))
-        print("#"*40)
+# To get a tuple with a single value then we add the value then add a coma with the end bracket.
+# for row in conn.execute("SELECT * FROM contacts WHERE name=?", (name,)):
+# Not case sensitive and will find something like what you are looking for
+for row in conn.execute("SELECT * FROM contacts WHERE name LIKE ?", (name,)):
+
+    print("#"*40)
+    print(row)
+    print("#"*40)
 conn.close()
