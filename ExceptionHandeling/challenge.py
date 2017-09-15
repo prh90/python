d60a8d43a834a5da0e18cf282c805f4ac49b8bd1
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-09-14 21:50:28 -0700

    Adds error handling and exit codes with sys

--- ExceptionHandeling/challenge.py
+++ ExceptionHandeling/challenge.py
@@ -10,11 +10,24 @@
 # except ZeroDivisionError:
 #     print("Cant divide by zero son")
 ###########################################################################
+import sys
 
-def getint():
+def getint(prompt):
     while True:
         try:
-            number = int(input("Please enter a number: "))
+            number = int(input(prompt))
             return number
         except ValueError:
             print("Invalid number entered, please try again")
+        except EOFError:
+            sys.exit(1)
+
+
+first_number = getint("Please enter first number: ")
+second_number = getint("Please enter second number: ")
+
+try:
+    print("{} divided by {} is {}".format(first_number, second_number, first_number / second_number))
+except ZeroDivisionError:
+    print("Cant divide by zero son")
+    sys.exit(2)
