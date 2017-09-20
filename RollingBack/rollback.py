b8f6af72a17754f7026c45d781e00fbf0899cf3a
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-09-19 18:54:53 -0700

    Adds fix to decimal output

--- RollingBack/rollback.py
+++ RollingBack/rollback.py
@@ -1,31 +1,33 @@
 class Account(object):
 
-    def __init__(self, name: str, opening_balance: float = 0.0):
+    def __init__(self, name: str, opening_balance: int = 0):
         self.name = name
         self._balance = opening_balance
-        print("Account create for {}.".format(self.name, end='')
+        print("Account created for {}.".format(self.name, end=''))
         self.show_balance()
 
-    def deposit(self, amount: float) -> float:
+    def deposit(self, amount: int) -> float:
             if amount > 0.0:
                 self._balance += amount
-                print("{} deposited".format(amount))
-            return self._balance
+                print("{:.2f} deposited".format(amount/100))
+            return self._balance/100
 
-    def withdraw(self, amount: float) -> float:
+    def withdraw(self, amount: int) -> float:
         if 0< amount <= self._balance:
             self._balance -= amount
-            print("{} withdrew".format(amount))
-            return amount
+            print("{:.2f} withdrew".format(amount/100))
+            return amount/100
         else:
             print("The amount must be greater than 0 and no more than your account balance")
 
     def show_balance(self):
-        print("Balance on {} account is {}".format(self.name, self._balance))
+        print("Balance on {}'s account is {:.2f}".format(self.name, self._balance / 100))
 
 
 if __name__ == '__main__':
     pablo = Account("Pablo")
-    pablo.deposit(100.00)
-    pablo.withdraw(57.89)
+    pablo.deposit(10000)
+    pablo.withdraw(57)
+    pablo.show_balance()
+    pablo.withdraw(42)
     pablo.show_balance()
