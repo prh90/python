6367881309cf508831628234b4af71e85ca1015f
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-09-21 02:10:27 -0700

    Adds refactored code to keep it simple adding new method to be called by other methods

--- RollingBack/rollback.py
+++ RollingBack/rollback.py
@@ -27,27 +27,37 @@ class Account(object):
             print("Account created for {}.".format(self.name, end=''))
         self.show_balance()
 
+    def _save_update(self, amount):
+        new_balance = self._balance + amount
+        deposit_time = Account._current_time()
+        db.execute("UPDATE accounts SET balance = ? WHERE (name=?)", (new_balance, self.name))
+        db.execute("INSERT INTO transactions VALUES(?, ?, ?)",(deposit_time, self.name, amount))
+        db.commit()
+        self._balance = new_balance
+
     def deposit(self, amount: int) -> float:
             if amount > 0.0:
                 # self._balance += amount
-                new_balance = self._balance + amount
-                deposit_time = Account._current_time()
-                db.execute("UPDATE accounts SET balance = ? WHERE (name=?)", (new_balance, self.name))
-                db.execute("INSERT INTO transactions VALUES(?, ?, ?)",(deposit_time, self.name, amount))
-                db.commit()
-                self._balance = new_balance
+                # new_balance = self._balance + amount
+                # deposit_time = Account._current_time()
+                # db.execute("UPDATE accounts SET balance = ? WHERE (name=?)", (new_balance, self.name))
+                # db.execute("INSERT INTO transactions VALUES(?, ?, ?)",(deposit_time, self.name, amount))
+                # db.commit()
+                # self._balance = new_balance
+                self._save_update(amount)
                 print("{:.2f} deposited".format(amount/100))
             return self._balance/100
 
     def withdraw(self, amount: int) -> float:
         if 0 < amount <= self._balance:
             # self._balance -= amount
-            new_balance = self._balance - amount
-            withdrawal_time = Account._current_time()
-            db.execute("UPDATE accounts SET balance = ? WHERE (name=?)", (new_balance, self.name))
-            db.execute("INSERT INTO transactions VALUES(?, ?, ?)", (withdrawal_time, self.name, -amount))
-            db.commit()
-            self._balance = new_balance
+            # new_balance = self._balance - amount
+            # withdrawal_time = Account._current_time()
+            # db.execute("UPDATE accounts SET balance = ? WHERE (name=?)", (new_balance, self.name))
+            # db.execute("INSERT INTO transactions VALUES(?, ?, ?)", (withdrawal_time, self.name, -amount))
+            # db.commit()
+            # self._balance = new_balance
+            self._save_update(-amount)
             print("{:.2f} withdrew".format(amount/100))
             return amount/100
         else:
