ed3b638a79a745abccd901596005f8a36e3416f6
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2017-10-04 23:11:41 -0700

    Adds kwargs

--- MusicBrowser/star_args.py
+++ MusicBrowser/star_args.py
@@ -24,15 +24,38 @@
 # def print_backwards(*args, file=None):
 #     for word in args[::-1]:
 #         print(word[::-1], end=' ', file=file)
+
+
+# Could pass it in as arg and use it
+# def print_backwards(*args, end=' ',**kwargs):
+# can remove it from dictionary kwargs
+# def print_backwards(*args, **kwargs):
+#     kwargs.pop('end', None)
+#     print(kwargs)
+#     for word in args[::-1]:
+#         print(word[::-1], end, **kwargs)
+
 def print_backwards(*args, **kwargs):
+    end_character = kwargs.pop('end', '\n')
+    sep_character = kwargs.pop('sep', ' ')
     print(kwargs)
-    for word in args[::-1]:
-        print(word[::-1], end=' ', **kwargs)
-
-
+    for word in args[:0:-1]:
+        print(word[::-1], end=sep_character, **kwargs)
+    # print(end=end_character)
+    print(args[0][::-1], end=end_character, **kwargs)
 
 with open("backwards.txt", "w") as backwards:
-    print_backwards("Hello", "Planet", "Earth", file=backwards)
+    print_backwards("Hello", "Planet", "Earth", end="\n")
+    print("Another string")
+
+
+print("*"*50)
+print("Hello", "Planet", "Earth", end='', sep='\n**\n')
+print_backwards("Hello", "Planet", "Earth", end='', sep='\n**\n')
+print("*"*50)
+
+
+
 
 # *args unpacks a tuple or a list
 # **kwargs unpacks a dictionary
