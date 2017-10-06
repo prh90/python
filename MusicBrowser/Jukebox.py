f0437e0131d2cc76318cd09182a84b05db7d6dc6
Pablo Hernandez <prh90@yahoo.com>
2017-10-06 00:17:40 -0700

    Adds the selection process for each table. Including reset, one problem is error with tuple in curselection

--- MusicBrowser/Jukebox.py
+++ MusicBrowser/Jukebox.py
@@ -25,7 +25,9 @@ class Scrollbox(tkinter.Listbox):
 def get_albums(event):
     lb = event.widget
     index = lb.curselection()[0]
+    print(index)
     artist_name = lb.get(index),
+    print(artist_name)
 
     # get the artist ID from the database row
     artist_id = conn.execute("SELECT artists._id FROM artists WHERE artists.name=?", artist_name).fetchone()
@@ -33,6 +35,24 @@ def get_albums(event):
     for row in conn.execute("SELECT albums.name FROM albums WHERE albums.artist = ? ORDER BY albums.name", artist_id):
         alist.append(row[0])
     albumLV.set(tuple(alist))
+    songLV.set(("Choose an album",))
+
+
+def get_songs(event):
+    lb = event.widget
+    print(lb.curselection()[0])
+    index = int(lb.curselection()[0])
+    print(index)
+    album_name = lb.get(index),
+    print(album_name)
+
+    # get the artist ID from the database
+    album_id = conn.execute("SELECT albums._id FROM albums WHERE albums.name=?", album_name).fetchone()
+    alist = []
+    for x in conn.execute("SELECT songs.title FROM songs WHERE songs.album = ? ORDER BY songs.track", album_id):
+        alist.append(x[0])
+    songLV.set(tuple(alist))
+
 
 mainWindow = tkinter.Tk()
 mainWindow.title("Music DB Browswer")
@@ -73,6 +93,8 @@ albumLV.set(("Choose an artist",))
 albumList = Scrollbox(mainWindow, listvariable=albumLV)
 albumList.grid(row=1, column=1, sticky='nsew', padx=(30, 0))
 albumList.config(border=2, relief='sunken')
+
+albumList.bind('<<ListboxSelect>>', get_songs)
 
 
 # ===== Songs Listbox =====
