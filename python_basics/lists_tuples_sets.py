d1cd65c81c1dc104ee52456f5e1fc08413143558
Pablo Hernandez <prh90@yahoo.com>
2017-10-13 20:32:54 -0700

    Adds practice with lists, tuples and sets

---
+++ python_basics/lists_tuples_sets.py
@@ -0,0 +1,8 @@
+# Most flexible can modify
+list_grades = [77, 80, 90, 95, 100]
+# tuples are inmutable - cannot increase size - no modification
+tuple_grades = (77, 80, 90, 95, 100)
+# set is unique and unordered
+set_grades = {77, 80, 90, 100, 100}
+
+print(sum(list_grades) / len(list_grades))
