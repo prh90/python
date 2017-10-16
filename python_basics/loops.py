db8e2a9d33a779b7bf25841f36bde8534c948227
Pablo Hernandez <prh90@yahoo.com>
2017-10-16 16:29:02 -0700

    Adds new file to practice loops

---
+++ python_basics/loops.py
@@ -0,0 +1,17 @@
+my_variable = "hello, world"
+
+# print(my_variable[0])
+
+# for character in my_variable:
+# iterables are strings, lists, sets, tuples and more
+#     print(character)
+
+my_list = [1, 3, 5, 7, 9]
+
+# for i in my_list:
+#     print(i ** 2)
+
+user_wants_number = True
+
+while user_wants_number == True:
+    print(10)
