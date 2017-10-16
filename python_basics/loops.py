71fe035fa36759fbed76e45326d20386580ad51b
Pablo Hernandez <prh90@yahoo.com>
2017-10-16 16:42:54 -0700

    Adds work with while loop with user input

--- python_basics/loops.py
+++ python_basics/loops.py
@@ -15,3 +15,11 @@ user_wants_number = True
 
 while user_wants_number == True:
     print(10)
+
+    user_input = input("Should we print again? (y/n) ")
+    # if user_input == "y":
+    #     user_wants_number = True
+    # elif user_input == "n":
+    #     user_wants_number = False
+    if user_input == "n":
+        user_wants_number = False
