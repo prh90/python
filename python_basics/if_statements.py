3306979930415b96d9c66c85775c7b484db051f8
Pablo Hernandez <prh90@yahoo.com>
2017-10-16 17:46:22 -0700

    adds work with if statements and format

---
+++ python_basics/if_statements.py
@@ -0,0 +1,12 @@
+# should_continue = True
+#
+# if should_continue == True:
+#     print("Hello")
+
+known_people = ["John", "Anna", "Mary"]
+person = input("Enter the person you know: ")
+
+if person in known_people:
+    print("You know {}!".format(person))
+else:
+    print("You don't know {}!".format(person))
