e8533338b050008d45438befc326665330a7ed12
Pablo Hernandez <prh90@yahoo.com>
2017-10-17 20:29:33 -0700

    adds cleaner code knowing how to use list in one line

--- python_basics/list_comprehension.py
+++ python_basics/list_comprehension.py
@@ -6,4 +6,8 @@ multiply_list = [x * 3 for x in range(5)]
 # print(multiply_list)
 # print(11 % 3)  # = 2 why? 9 fits into 11 and remainder 2
 
-print([n for n in range(10) if n % 2 == 0])
+# print([n for n in range(10) if n % 2 == 0])
+
+people_you_know = ["Rolf", " John", "anna", "GREG"]
+normalised_people = [person.strip().lower() for person in people_you_know]
+print(normalised_people)
