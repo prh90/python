7ff2d1c073a077d22d72c453ac34339ec946e038
Pablo Hernandez <prh90@yahoo.com>
2017-10-18 17:48:58 -0700

    Adds nothing

--- python_basics/dictionaries.py
+++ python_basics/dictionaries.py
@@ -1,5 +1,6 @@
 my_set = {1, 3, 5}
-my_dict = {'name': 'Jose', 'age': 90, 'grades': [13, 45, 66, 90]}  # list within dicitonary
+# list within dicitonary
+my_dict = {'name': 'Jose', 'age': 90, 'grades': [13, 45, 66, 90]}
 another_dict = {1: 15, 2: 75, 3: 150}
 
 lottery_player = {
@@ -7,17 +8,20 @@ lottery_player = {
     'numbers': (13, 45, 66, 23, 22)
 }
 # List of dictionaries
-universities = {
-    {
-        'name': 'Oxford',
-        'location': 'UK'
-    },
-    {
-        'name': 'MIT',
-        'location': 'US'
-    }
-}
+# universities = {
+#     {
+#         'name': 'Oxford',
+#         'location': 'UK'
+#     },
+#     {
+#         'name': 'MIT',
+#         'location': 'US'
+#     }
+# }
 
-sum(lottery_player['numbers'])  # Can access a key
+# sum(lottery_player['numbers'])  # Can access a key
 
-lottery_player['name'] = 'John'  # Reassign
+# lottery_player['name'] = 'John'  # Reassign
+
+##
+print(sum(my_dict['grades']))
