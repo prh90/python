86322aff0822bf2aed4021ec48633897673d6e2d
Pablo Hernandez <prh90@yahoo.com>
2017-10-19 22:23:44 -0700

    Adds work with inheritance

--- python_basics/inheritance.py
+++ python_basics/inheritance.py
@@ -6,4 +6,31 @@ class Student:
 
     def average(self):
         return sum(self.marks) / len(self.marks)
-    
+
+    @classmethod
+    def friend(cls, origin, friend_name, salary):
+        return cls(friend_name, origin.school, salary)
+
+
+# anna = Student("Anna", "Oxford")
+# friend = anna.friend("Greg")
+# print(friend.name)
+# print(friend.school)
+
+##
+
+
+class WorkingStudent(Student):
+
+    def __init__(self, name, school, salary):
+        super().__init__(name, school)
+        self.salary = salary
+
+
+anna = WorkingStudent("Anna", "Oxford", 20.00)
+print(anna.salary)
+
+friend = WorkingStudent.friend(anna, "Greg")
+print(friend.name)
+print(friend.school)
+print(friend.salary)
