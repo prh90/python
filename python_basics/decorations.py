d72e827bef6f1be90b65f2710ce1adfb3a4bf83b
Pablo Hernandez <prh90@yahoo.com>
2017-10-23 18:32:53 -0700

    Adds two templates for decorators

--- python_basics/decorations.py
+++ python_basics/decorations.py
@@ -15,4 +15,30 @@ def my_function():
     print("I'm the function")
 
 
-my_function()
+# my_function()
+
+
+####
+def decorator_with_arguments(number):
+    def my_decorator(func):
+        @functools.wraps(func)
+        def function_that_runs_func(*args, **kwargs):
+            print("In the decorator")
+            if number == 56:
+                print("Not running the function")
+            else:
+                func(*args, **kwargs)
+            print("After the decorator")
+        return function_that_runs_func
+    return my_decorator
+
+
+# Useful for user permission for websites
+# insert elements into db
+@decorator_with_arguments(6)
+def my_function_too(x, y):
+    # print("Hello")
+    print(x + y)
+
+
+my_function_too(57, 67)
