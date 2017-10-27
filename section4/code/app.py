059ba5b01746df770401388bcd72cc174ddcf68b
Pablo Hernandez <prh90@yahoo.com>
2017-10-26 20:45:48 -0700

    Adds cleaner, shorter code for get and post

--- section4/code/app.py
+++ section4/code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -8,18 +8,31 @@ items = []
 
 
 class Item(Resource):
+
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        # None at the end is if next cant find anything else it returns none
+        # for item in items:
+        #     if item['name'] == name:
+        #         return item
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message': "An item with the name '{}' already exists".format(name)}, 400
+            
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
 
-api.add_resource(Item, '/item/<string:name>')  # How we access
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
-app.run(port=5000)
+
+api.add_resource(Item, '/item/<string:name>')  # How we access
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
