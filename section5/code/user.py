1eb14ffa14a4bd22b45c38e4275fc4d60866a238
Pablo Hernandez <prh90@yahoo.com>
2017-10-31 20:52:26 -0700

    Adds db and parser to user register

--- section5/code/user.py
+++ section5/code/user.py
@@ -1,4 +1,6 @@
 import sqlite3
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
 
 
 class User:
@@ -41,7 +43,22 @@ class User:
 
 
 class UserRegister(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+        type=str,
+        required=True,
+        help="This field cannot be blank."
+        )
+    parser.add_argument('password',
+        type=str,
+        required=True,
+        help="This field cannot be blank."
+        )
+
     def post(self):
+        data = UserRegister.parser.parse_args()
+        
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
