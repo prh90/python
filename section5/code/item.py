a01d53a719394569b8a835da42299555865648b1
Pablo Hernandez <prh90@yahoo.com>
2017-11-01 21:47:28 -0700

    Adds better use of the methods with worh with exceptions and sql work

--- section5/code/item.py
+++ section5/code/item.py
@@ -13,43 +13,108 @@ class Item(Resource):
 
     @jwt_required()  # can use it for any method that needs auth
     def get(self, name):
+        # connection = sqlite3.connect('data.db')
+        # cursor = connection.cursor()
+        #
+        # query = "SELECT * FROM items WHERE name=?"
+        # # item = next(filter(lambda x: x['name'] == name, items), None)
+        # # None at the end is if next cant find anything else it returns none
+        # # return {'item': item}, 200 if item else 404
+        # result = cursor.execute(query, (name,))
+        # row = result.fetchone()
+        # connection.close()
+        # if row:
+        #     return {'item': {'name': row[0], 'price': row[1]}}
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {'message': 'Item not found'}, 404
+
+    @classmethod
+    def find_by_name(cls, name):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
         query = "SELECT * FROM items WHERE name=?"
-        # item = next(filter(lambda x: x['name'] == name, items), None)
-        # # None at the end is if next cant find anything else it returns none
-        # return {'item': item}, 200 if item else 404
         result = cursor.execute(query, (name,))
         row = result.fetchone()
         connection.close()
         if row:
             return {'item': {'name': row[0], 'price': row[1]}}
-        return {'message': 'Item not found'}, 404
+
+
 
     def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+        if self.find_by_name(name):
             return {'message': "An item with the name '{}' already exists".format(name)}, 400
+
+        # if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            # return {'message': "An item with the name '{}' already exists".format(name)}, 400
 
         data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
-        items.append(item)
+
+        try:
+            self.insert(item)
+        except:
+            return {"message": "An error has occured inserting the item."}, 500
+            # 500 = internal server error
         return item, 201
 
+    @classmethod
+    def insert(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
+
     def delete(self, name):
-        global items  # if not declared it will think items is only local
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message': 'Item deleted'}, 200
+        # items = list(filter(lambda x: x['name'] != name, items))
+        if self.find_by_name(name):
+            connection = sqlite3.connect('data.db')
+            cursor = connection.cursor()
+
+            query = "DELETE FROM items WHERE name=?"
+            cursor.execute(query, (name,))
+
+            connection.commit()
+            connection.close()
+
+            return {'message': 'Item deleted'}, 200
+        return {'message': "Item not found, item not deleted"}, 404
 
     def put(self, name):
         data = Item.parser.parse_args()
-        item = next(filter(lambda x: x['name'] == name, items), None)
+        item = self.find_by_name(name)
+        # item = next(filter(lambda x: x['name'] == name, items), None)
+        updated_item = {'name': name, 'price': data['price']}
+
         if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
+            try:
+                self.insert(updated_item)
+            except:
+                return {"message": "An error occured inserting the item"}, 500
         else:
-            item.update(data)
-        return item, 200
+            try:
+                self.update(updated_item)
+            except:
+                return {"message": "An error occured updating the item"}, 500
+        return updated_item, 200
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (item['price'], item['name']))
+
+        connection.commit()
+        connection.close()
 
 
 class ItemList(Resource):
