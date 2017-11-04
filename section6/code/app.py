8ef57f69a07da3b7bd91e1a93ef134a25401f5b9
Pablo Hernandez <prh90@yahoo.com>
2017-11-03 20:15:41 -0700

    Adds the transition to ALCHEMY

--- section6/code/app.py
+++ section6/code/app.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
+
 from security import authenticate, identity
 from resources.user import UserRegister  # Its like resources/user
 from resources.item import Item, ItemList
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Wont log anything
 app.secret_key = 'pablo'
 api = Api(app)
 
@@ -17,4 +20,6 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
     app.run(port=5000, debug=True)
