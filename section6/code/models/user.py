b1da5e4d7c3fc8914473396ebd6970a9150401d3
Pablo Hernandez <prh90@yahoo.com>
2017-11-03 22:11:53 -0700

    Adds refactored code for user models

--- section6/code/models/user.py
+++ section6/code/models/user.py
@@ -16,32 +16,35 @@ class UserModel(db.Model):
 
     @classmethod
     def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query, (username,))  # single value tuple
-        row = result.fetchone()
-        if row:
-            user = cls(*row)  # displays the whole row
-        else:
-            user = None
-
-        connection.close()
-        return user
+        return cls.query.filter_by(username=username).first()
+        # connection = sqlite3.connect('data.db')
+        # cursor = connection.cursor()
+        #
+        # query = "SELECT * FROM users WHERE username=?"
+        # result = cursor.execute(query, (username,))  # single value tuple
+        # row = result.fetchone()
+        # if row:
+        #     user = cls(*row)  # displays the whole row
+        # else:
+        #     user = None
+        #
+        # # connection.close()
+        # return user
 
     @classmethod
     def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+        return cls.query.filter_by(id=_id).first()
 
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query, (_id,))  # single value tuple
-        row = result.fetchone()
-        if row:
-            user = cls(*row)  # displays the whole row
-        else:
-            user = None
-
-        connection.close()
-        return user
+        # connection = sqlite3.connect('data.db')
+        # cursor = connection.cursor()
+        #
+        # query = "SELECT * FROM users WHERE id=?"
+        # result = cursor.execute(query, (_id,))  # single value tuple
+        # row = result.fetchone()
+        # if row:
+        #     user = cls(*row)  # displays the whole row
+        # else:
+        #     user = None
+        #
+        # connection.close()
+        # return user
