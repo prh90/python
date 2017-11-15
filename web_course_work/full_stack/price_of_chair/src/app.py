07a150aab46177e9cba3881cead002ceb235ce5c
Pablo Hernandez <prh90@yahoo.com>
2017-11-15 00:50:12 -0800

    Adds float to compare price and add if else statement

--- web_course_work/full_stack/price_of_chair/src/app.py
+++ web_course_work/full_stack/price_of_chair/src/app.py
@@ -7,4 +7,13 @@ soup = BeautifulSoup(content, "html.parser")
 element = soup.find("span", {"itemprop": "price", "class": "now-price"})
 # <span itemprop="price" class="now-price">£129.00</span>
 
-print(element.text.strip().pop())
+string_price = element.text.strip()  # "£129.00"
+
+price_without_symbol = string_price[1:]
+
+price = float(price_without_symbol)
+
+if price < 200:
+    print("Buy the chair, its only {}".format(string_price))
+else:
+    print("Do not buy yet its {}".format(string_price))
