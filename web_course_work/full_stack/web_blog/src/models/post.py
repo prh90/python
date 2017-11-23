8e7d9aa9d39456a210efff7ad79834e39bace00e
Pablo Hernandez <prh90@yahoo.com>
2017-11-22 18:44:44 -0800

    Adds refactored code like cls method to include **blog_data

--- web_course_work/full_stack/web_blog/src/models/post.py
+++ web_course_work/full_stack/web_blog/src/models/post.py
@@ -1,23 +1,23 @@
 import uuid
-from database import Database
+from src.common.database import Database
 import datetime
 
 
 class Post(object):
-    def __init__(self, blog_id, title, content, author, date=datetime.datetime.utcnow(), id=None):
+    def __init__(self, blog_id, title, content, author, created_date=datetime.datetime.utcnow(), _id=None):
         self.blog_id = blog_id
         self.title = title
         self.content = content
         self.author = author
-        self.created_date = date
-        self.id = uuid.uuid4().hex if id is None else id
+        self.created_date = created_date
+        self._id = uuid.uuid4().hex if _id is None else _id
 
     def save_to_mongo(self):
         Database.insert(collection='posts', data=self.json())
 
     def json(self):
         return {
-            'id': self.id,
+            '_id': self._id,
             'blog_id': self.blog_id,
             'author': self.author,
             'content': self.content,
@@ -27,13 +27,8 @@ class Post(object):
 
     @classmethod
     def from_mongo(cls, id):
-        post_data = Database.find_one(collection='posts', query={'id': id})
-        return cls(blog_id=post_data['blog_id'],
-                   title=post_data['title'],
-                   content=post_data['content'],
-                   author=post_data['author'],
-                   date=post_data['date'],
-                   id=post_data['id'])
+        post_data = Database.find_one(collection='posts', query={'_id': id})
+        return cls(**post_data)  # unpacks to match
 
     @staticmethod
     def from_blog(id):
