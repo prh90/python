919e1af9da1c6121c677dec3fbb8a368ebe83f91
Pablo Hernandez <prh90@yahoo.com>
2017-11-22 21:09:18 -0800

    Adds User class with better use of methods

--- web_course_work/full_stack/web_blog/src/models/user.py
+++ web_course_work/full_stack/web_blog/src/models/user.py
@@ -1,22 +1,53 @@
+import uuid
+from src.common.database import Database
+
+
 class User(object):
-    def __init__(self):
-        pass
+    def __init__(self, email, password, _id):
+        self.email = email
+        self.password = password
+        self._id = uuid.uuid4().hex if _id is None else _id
 
-    def get_by_email(self):
-        pass
+    @classmethod
+    def get_by_email(cls, email):
+        data = Database.find_one("users", {"email": email})
+        if data is not None:
+            return cls(**data)
 
-    def get_by_id(self):
-        pass
+    @classmethod
+    def get_by_id(cls, _id):
+        data = Database.find_one("users", {"_id": _id})
+        if data is not None:
+            return cls(**data)
 
-    def login_valid(self):
+    @staticmethod
+    def login_valid(email, password):
         # chekc whether a users email matches the password they sent us
-        pass
+        user = User.get_by_email(email)
+        if user is not None:
+            return user.password == password
+        return False
 
-    def register(self):
-        pass
+    @classmethod
+    def register(cls, email, password):
+        user = cls.get_by_email(email)
+        if user is None:
+            new_user = cls(email, password)
+            new_user.save_to_mongo()
+            #  User doesnt exist, so we can create it
+            return True
+        else:
+            return False
+    #         User exists
 
     def login(self):
         pass
 
     def get_blogs(self):
+        pass
+
+    def json(self):
+        pass
+
+    def save_to_mongo(self):
         pass
