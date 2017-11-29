87ce5fbce4db3bd58bb67d873644f56d21dc97f3
Pablo Hernandez <prh90@yahoo.com>
2017-11-28 23:52:55 -0800

    Adds new routes and css

--- web_course_work/full_stack/web_blog/src/app.py
+++ web_course_work/full_stack/web_blog/src/app.py
@@ -5,10 +5,19 @@ from src.models.user import User
 app = Flask(__name__)  # __main__
 app.secret_key = "pablo"
 
-
-@app.route('/')  # www.mysite.com/api/
+@app.route('/')
 def home():
+    return render_template('home.html')
+
+
+@app.route('/login')  # 127.0.0.1:5000/login
+def login_template():
     return render_template('login.html')
+
+
+@app.route('/register')  # 127.0.0.1:5000/register
+def register_template():
+    return render_template('register.html')
 
 
 @app.before_first_request
@@ -16,7 +25,7 @@ def initialize_database():
     Database.initialize()
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/auth/login', methods=['POST'])
 def login():
     email = request.form['email']
     password = request.form['password']
@@ -25,6 +34,16 @@ def login():
         User.login(email)
     else:
         session['email'] = "Guest"
+
+    return render_template("profile.html", email=session['email'])
+
+
+@app.route('/auth/register', methods=['POST'])
+def register():
+    email = request.form['email']
+    password = request.form['password']
+
+    User.register(email, password)
 
     return render_template("profile.html", email=session['email'])
 
