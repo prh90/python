6712b10372688b456d9e543fded577573fcfacfe
Pablo Hernandez <prh90@yahoo.com>
2017-11-30 02:17:29 -0800

    Adds blueprint for views for all models including routes

---
+++ web_course_work/full_stack/project/src/models/items/views.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+
+item_blueprint = Blueprint('items', __name__)
+
+
+@item_blueprint.route('/item/<string:name>')
+def item_page():
+    pass
+
+@item_blueprint.route('/load')
+def load_item():
+    """
+    Load an item's data using their store and return JSON representation of it
+    :return:
+    """
+    pass
