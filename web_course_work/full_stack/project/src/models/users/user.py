7a60e10d8f412cbfd1399f4572df951f81dd0333
Pablo Hernandez <prh90@yahoo.com>
2017-11-30 02:55:22 -0800

    Adds methods for connecting db and a few methods for user

--- web_course_work/full_stack/project/src/models/users/user.py
+++ web_course_work/full_stack/project/src/models/users/user.py
@@ -1,7 +1,25 @@
+import uuid
+
+from src.common.database import Database
+
+
 class User(object):
-    def __init__(self, email, password):
+    def __init__(self, email, password, _id=None):
         self.email = email
         self.password = password
+        self._id = uuid.uuid4().hex if _id is None else _id
 
     def __repr__(self):
         return "<User {}>".format(self.email)
+
+    @staticmethod
+    def is_login_valid(email, password):
+        """
+        This method verifies that email/password combo is valid or not
+        Check if the email exists and that the password associated to that email is correct
+        """
+        user_data = Database.find_one("users", {"email": email})
+        if user_data is None:
+            pass
+        if not Utils.check_hashed_password(password, user_data['password']):
+            pass
