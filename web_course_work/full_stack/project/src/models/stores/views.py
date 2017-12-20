7d74f706cc16fec7eb6e055430bc1498ab6668e3
Pablo Hernandez <prh90@yahoo.com>
2017-12-19 22:20:30 -0800

    Adds store new page with a new route

--- web_course_work/full_stack/project/src/models/stores/views.py
+++ web_course_work/full_stack/project/src/models/stores/views.py
@@ -1,13 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 store_blueprint = Blueprint('stores', __name__)
 
 
 @store_blueprint.route('/')
 def index():
-    return "This is the store index"
+    stores = []
+    return render_template('stores/store_index.html')
 
 
 @store_blueprint.route('/store/<string:name>')
 def store_page():
     pass
+
+@store_blueprint.route('/new', methods=['GET', 'POST'])
+def create_store():
+    return "This is the store creation page"
