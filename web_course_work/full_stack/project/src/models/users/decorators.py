8981560ccb7144e19a21202b0223836124ce7514
Pablo Hernandez <prh90@yahoo.com>
2017-12-26 19:14:02 -0800

    Adds fix to email output with more info for user

--- web_course_work/full_stack/project/src/models/users/decorators.py
+++ web_course_work/full_stack/project/src/models/users/decorators.py
@@ -1,6 +1,5 @@
 from functools import wraps
-from flask import session, url_for, request, redirect
-from src.app import app
+from flask import session, url_for, request, redirect, current_app
 
 
 def requires_login(func):
@@ -20,7 +19,7 @@ def requires_admin_permissions(func):
     def decorated_function(*args, **kwargs):
         if 'email' not in session.keys() or session['email'] is None:
             return redirect(url_for('users.login_user', next=request.path))
-        if session['email'] not in app.config.ADMINS:
+        if session['email'] not in current_app.config.ADMINS:
             return redirect(url_for('users.login_user'))
         return func(*args, **kwargs)
 
